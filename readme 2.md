db47fcb0f2e4089fae603d8e66f89344eb82c84c
Darryl Davis <ddavis1306@gmail.com>
2017-11-07 17:26:40 -0800

    moved files over from 16

---
+++ readme 2.md
@@ -0,0 +1,3 @@
+
+* how to sign in `http :5000/signin -a ddavis:mypass`
+* how to sign up `echo '{"username":"vis", "password":"mypass"}' | http :5000/signup`
---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/auth_dev', {useMongoClient: true});
+
+const app = require('express')();
+app.use(require('./routes/auth-routes'));
+app.use((err, req, res, next) => {
+  res.status(500 || err.statusCode).send(err.message || 'server error');
+});
+app.listen(process.env.PORT || 5500);
+
+module.exports = {
+  start: (port, cb) => {
+    app.listen(port, cb);
+    console.log(`Server is running on PORT: ${process.env.PORT}`);
+  },
+  stop: (cb) => app.close(cb),
+};
